b4a7f580c20eec066b905ab1e568539b3e5263f7
klaraszc <theklaracja@gmail.com>
2022-10-29 09:57:06 +0200

    nowa

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+#druga linijka komentarza
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+defprint(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+ print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
